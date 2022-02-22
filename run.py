29b9ed52b498d4230dedb9e6025355fcdbcb3611
Vannesha <vannesha.dev@gmail.com>
2022-02-22 18:03:00 +0000

    Create get_month_data function to get the month from the user

--- run.py
+++ run.py
@@ -12,7 +12,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('wedding-savings')
 
-all_data = SHEET.worksheet('all_data')
-all_data = all_data.get_all_values()
-print(all_data)
 
+def get_month_data():
+
+    """
+    Get month from the user.
+
+    """
+    print("Welcome to Wedding Savings Planner.\n")
+    print("Please state the month in terms of a number"),
+    print("You may choose from 1 to 12.\n"),
+    print("Example: If it is February, you should state 2\n")
+
+    month = input("Enter the month here:\n")
+
+
+def main():
+
+    """
+    Run all program functions
+    """
+    get_month_data()
+
+
+main()
