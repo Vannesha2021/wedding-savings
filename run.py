aaa2e3c590b4566883a6d273d5083507b96bd49c
Vannesha <vannesha.dev@gmail.com>
2022-02-24 00:08:06 +0000

    Add source code to obtain the month-May, fetch the expenses and savings data from the worksheet and calculate the total savings for the past and present month/s before deduction of all expenses

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ def get_month_data():
             show_alldata_month2(month)
             show_alldata_month3(month)
             show_alldata_month4(month)
+            show_alldata_month5(month)
             break
 
 
@@ -173,6 +174,7 @@ def show_alldata_month3(month):
 
     return month
 
+
 def show_alldata_month4(month):
 
     """
@@ -213,6 +215,52 @@ def show_alldata_month4(month):
         print(("Your total savings before expenses year to date is:"), apr_ts)
 
     return month
+
+def show_alldata_month5(month):
+
+    """
+    Projects expenses,savings and balance for
+    May
+    """
+
+    if month == "5":
+        alldata_m1_1 = SHEET.worksheet("all_data").get('A17')[0][0]
+        print(("The month you have chosen :"), alldata_m1_1)
+
+        alldata_m1_2 = SHEET.worksheet("all_data").get('B18')[0][0]
+        print(("Your expenses for this month will be:"), alldata_m1_2)
+
+        alldata_m1_3 = SHEET.worksheet("all_data").get('B19')[0][0]
+        print("\n")
+        print("Retrieving savings data...\n")
+
+        may = int(alldata_m1_3)
+        print(("Your savings for this month will be:"), may)
+        
+        """
+        Projects savings after deduction of expenses
+        """
+        may = int(alldata_m1_3) - int(alldata_m1_2)
+        print(("Your savings after expenses will be:"), may)
+
+        print("\n")
+        print("Retrieving total savings data...\n")
+
+        """
+        Projects overall savings for past and present month/s 
+        before deduction of all monthly expenses
+        """
+
+        alldata_m1_4 = SHEET.worksheet("all_data").get('B3')[0][0]
+        alldata_m1_5 = SHEET.worksheet("all_data").get('B7')[0][0]
+        alldata_m1_6 = SHEET.worksheet("all_data").get('B11')[0][0]
+        alldata_m1_7 = SHEET.worksheet("all_data").get('B15')[0][0]
+        may_ts = (int(alldata_m1_3) + int(alldata_m1_4) + int(alldata_m1_5) +
+                  int(alldata_m1_6) + int(alldata_m1_7))
+        print(("Your total savings before expenses year to date is:"), may_ts)
+
+    return month
+
 
 def main():
 
